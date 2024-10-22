a23e5803d9670674f5fc95dd66f9b2a759ce5ae9
alin36 <amyli2191@gmail.com>
2024-10-22 14:50:30 -0500

    Add files via upload

---
+++ AppleByTheWindows/AppleByTheWindows/src/main/java/com/ABTW/AppleByTheWindows/Transactions.java
@@ -0,0 +1,21 @@
+package com.ABTW.AppleByTheWindows;
+
+public class Transactions {
+    //class fields
+    private String cardNumber;
+    private String date;
+    private String type;
+    private double amount;
+    private double balance;
+
+    //default constructor
+
+    public Transactions(String cardNumber, String date, String type, double amount) {
+        this.cardNumber = cardNumber;
+        this.date = date;
+        this.type = type;
+        this.amount = amount;
+        this.balance = 0;
+    }
+
+}
