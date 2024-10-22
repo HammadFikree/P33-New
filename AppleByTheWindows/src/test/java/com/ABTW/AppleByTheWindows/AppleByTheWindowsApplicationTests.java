2caeea4f64730caa56fea2fd4c92527694e1bfa7
alin36 <amyli2191@gmail.com>
2024-10-22 14:55:53 -0500

    Add files via upload

---
+++ AppleByTheWindows/src/test/java/com/ABTW/AppleByTheWindows/AppleByTheWindowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABTW.AppleByTheWindows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleByTheWindowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
